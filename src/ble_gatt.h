ce2bcc282889c2182f2c894553f51225d04c66d0
Magnus Thomé <32169384+MagnusThome@users.noreply.github.com>
2019-01-11 14:07:17 +0100

    Moved data around in GATT data packets

--- src/ble_gatt.h
+++ src/ble_gatt.h
@@ -1,34 +1,29 @@
 #include <bluefruit.h>
 
-BLEService        mainService         = BLEService        (0x00000001000000fd8933990d6f411ff7);
-BLECharacteristic distCharacteristics = BLECharacteristic (0x01);
-BLECharacteristic tempCharacteristics = BLECharacteristic (0x02);
-BLECharacteristic battCharacteristics = BLECharacteristic (0x03);
+BLEService  mainService   = BLEService        (0x00000001000000fd8933990d6f411ff7);
+BLECharacteristic GATTone = BLECharacteristic (0x01);
+BLECharacteristic GATTtwo = BLECharacteristic (0x02);
 
 
 
 void setupMainService(void) {
+  
   mainService.begin();
 
-  distCharacteristics.setProperties(CHR_PROPS_NOTIFY | CHR_PROPS_READ);  // Options: CHR_PROPS_BROADCAST, CHR_PROPS_NOTIFY, CHR_PROPS_INDICATE, CHR_PROPS_READ, CHR_PROPS_WRITE_WO_RESP, CHR_PROPS_WRITE
-  distCharacteristics.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
-  distCharacteristics.setFixedLen(2);
-  distCharacteristics.begin();
+  GATTone.setProperties(CHR_PROPS_NOTIFY | CHR_PROPS_READ);  // Options: CHR_PROPS_BROADCAST, CHR_PROPS_NOTIFY, CHR_PROPS_INDICATE, CHR_PROPS_READ, CHR_PROPS_WRITE_WO_RESP, CHR_PROPS_WRITE
+  GATTone.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  GATTone.setFixedLen(20);
+  GATTone.begin();
 
-  tempCharacteristics.setProperties(CHR_PROPS_NOTIFY | CHR_PROPS_READ);
-  tempCharacteristics.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
-  tempCharacteristics.setFixedLen(16);
-  tempCharacteristics.begin();
-
-  battCharacteristics.setProperties(CHR_PROPS_NOTIFY | CHR_PROPS_READ);
-  battCharacteristics.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
-  battCharacteristics.setFixedLen(4);
-  battCharacteristics.begin();
+  GATTtwo.setProperties(CHR_PROPS_NOTIFY | CHR_PROPS_READ);
+  GATTtwo.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  GATTtwo.setFixedLen(20);
+  GATTtwo.begin();
 }
 
 
-void startAdvertising(void)
-{
+void startAdvertising(void) {
+  
   Bluefruit.setTxPower(4);
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
