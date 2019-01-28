2409f052c74ca7e180b62cc96aaff8a5943c9ca2
Magnus Thomé <32169384+MagnusThome@users.noreply.github.com>
2019-01-29 00:17:51 +0100

    false alarm on mV, reverted

--- main/adc_vbat.h
+++ main/adc_vbat.h
@@ -13,15 +13,16 @@ int getVbat() {
 
 
 uint8_t lipoPercent(float mvolts) {
-    if (mvolts >= 4190) { return 100; }
-    if (mvolts > 4090)  { return  90 + (mvolts-4090)*10/100; }
-    if (mvolts > 3990)  { return  80 + (mvolts-3990)*10/100; }
-    if (mvolts > 3890)  { return  60 + (mvolts-3890)*20/100; }
-    if (mvolts > 3790)  { return  40 + (mvolts-3790)*20/100; }
-    if (mvolts > 3690)  { return  10 + (mvolts-3690)*30/100; }
-    if (mvolts > 3590)  { return   0 + (mvolts-3590)*10/100; }
+    if (mvolts >= 4200) { return 100; }
+    if (mvolts > 4100)  { return  90 + (mvolts-4100)*10/100; }
+    if (mvolts > 4000)  { return  80 + (mvolts-4000)*10/100; }
+    if (mvolts > 3900)  { return  60 + (mvolts-3900)*20/100; }
+    if (mvolts > 3800)  { return  40 + (mvolts-3800)*20/100; }
+    if (mvolts > 3700)  { return  10 + (mvolts-3700)*30/100; }
+    if (mvolts > 3600)  { return   0 + (mvolts-3600)*10/100; }
     return 0;
 }
 
 
 // ----------------------------------------
+
